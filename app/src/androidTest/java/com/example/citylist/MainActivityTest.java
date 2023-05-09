c9e1d247bf652339f315159fa07cb80fcdf630c9
utsho-anik <anikdas548@gmail.com>
2023-05-10 02:47:42 +0600

    ui-test

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -76,4 +76,21 @@ public class MainActivityTest {
         Espresso.pressBack(); //Back button
     }
 
+
+    @Test
+    public void labtest()
+    {
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        Espresso.pressBack();
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+        onView(withId(R.id.second)).check(matches(isDisplayed()));
+
+        onView(withText("Edmonton")).check(matches(isDisplayed()));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.main)).check(matches(isDisplayed()));
+    }
+
 }
